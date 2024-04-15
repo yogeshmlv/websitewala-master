1384d5be1c208a59842c803e7c4c38bc3cc4e7f5
Yogesh <yogesh@YOGESHs-MacBook-Air.local>
2024-04-15 20:40:35 +0530

    first-commit

---
+++ websitewala/src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './globals.css';
+
+function page() {
+  return (
+    <div className='text-center'>
+      <h1>Website Under Development</h1>
+    </div>
+  )
+}
+
+export default page
