f7fc894b8d31049b140447d640dd3a7d333ccb2e
Yogesh <yogesh@YOGESHs-MacBook-Air.local>
2024-06-14 01:38:35 +0530

    build-fail-fixes

--- websitewala/src/app/page.jsx
+++ websitewala/src/app/page.jsx
@@ -10,42 +10,40 @@ import HomePageBlog from "@/component/section/HomePageBlog";
 const Home = () => {
   return (
     <>
-    <section>
-    <div className={styles.container}>
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>Welcome to Gorakhpur Website Wala – Your Local Destination for Exceptional Web Solutions</h1>
-        <p className={styles.desc}>
-        Our team of expert designers and developers specializes in creating stunning,
-         responsive websites tailored to your unique requirements. Whether you're a small business,
-          a startup, or an established enterprise, we have the expertise to elevate your online presence and drive results.
-        </p>
-        <div className={styles.buttons}>
-          <Link href='/about'><button className={styles.button}>Learn More</button></Link>
-          <Link href="/contact">
-            <button style={contactButtonStyles}>Contact</button>
-          </Link>
+      <section>
+        <div className={styles.container}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>Welcome to Gorakhpur Website Wala – Your Local Destination for Exceptional Web Solutions</h1>
+            <p className={styles.desc}>
+              Our team of expert designers and developers specializes in creating stunning, responsive websites tailored to your unique requirements. Whether you&apos;re a small business, a startup, or an established enterprise, we have the expertise to elevate your online presence and drive results.
+            </p>
+            <div className={styles.buttons}>
+              <Link href='/about'><button className={styles.button}>Learn More</button></Link>
+              <Link href="/contact">
+                <button style={contactButtonStyles}>Contact</button>
+              </Link>
+            </div>
+          </div>
+          <div className={styles.imgContainer}>
+            <Image src="/hero.gif" alt="Hero Image" layout="fill" className={styles.heroImg} />
+          </div>
         </div>
-      </div>
-      <div className={styles.imgContainer}>
-        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
-      </div>
-    </div>
-    </section>
-    <section className={styles.sectionWho}>
-    <HomePageContent/>
-    </section>
-    <section className={styles.sectionWho}>
-    <HomePageContact/>
-    </section>
-    <section className={styles.sectionWho}>
-    <HomePageWhatDo/>
-    </section>
-    <section className={styles.sectionWho}>
-    <Testimonials/>
-    </section>
-    <section className={styles.sectionWho}>
-    <HomePageBlog/>
-    </section>
+      </section>
+      <section className={styles.sectionWho}>
+        <HomePageContent/>
+      </section>
+      <section className={styles.sectionWho}>
+        <HomePageContact/>
+      </section>
+      <section className={styles.sectionWho}>
+        <HomePageWhatDo/>
+      </section>
+      <section className={styles.sectionWho}>
+        <Testimonials/>
+      </section>
+      <section className={styles.sectionWho}>
+        <HomePageBlog/>
+      </section>
     </>
   );
 };
