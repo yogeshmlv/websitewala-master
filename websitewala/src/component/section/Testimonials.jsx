24a10410c105918edbda7f5176a768bb81a845a7
Yogesh <yogesh@YOGESHs-MacBook-Air.local>
2024-06-14 15:54:30 +0530

    fixes

--- websitewala/src/component/section/Testimonials.jsx
+++ websitewala/src/component/section/Testimonials.jsx
@@ -2,28 +2,27 @@
 import React from 'react';
 import { Container, Typography, Box, Card, CardContent, CardHeader, Avatar } from '@mui/material';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { createClient } from "contentful";
 
+const fetchTestimonials = async () => {
+  try {
+    const client = createClient({
+      space: 'h9ufuj4h46ir',
+      accessToken: '4nP7u4PUt9y6uCCaAOg1QSw5Hi8R4OOTVm1gNgbRZpA',
+    });
+
+    const res = await client.getEntries({ content_type: 'testimonials' });
+    return res.items;
+  } catch (error) {
+    console.error('Error fetching testimonials:', error);
+    return [];
+  }
+};
+
 const Testimonials = async () => {
-
-  const fetchTestimonials = async () => {
-      try {
-        const client = createClient({
-          space: 'h9ufuj4h46ir',
-          accessToken: '4nP7u4PUt9y6uCCaAOg1QSw5Hi8R4OOTVm1gNgbRZpA',
-        });
-      
-        const res = await client.getEntries({ content_type: 'testimonials' });
-        return res.items;
-      } catch (error) {
-        console.error('Error fetching testimonials:', error);
-        return []; 
-      }
-    };
-
-    const testimonials = await fetchTestimonials();
+  const testimonials = await fetchTestimonials();
 
   const settings = {
     dots: true,
@@ -35,34 +34,34 @@ const Testimonials = async () => {
     pauseOnHover: true,
     arrows: false,
     responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <Container maxWidth="xl" sx={{pb: '54px' }}>
+    <Container maxWidth="xl" sx={{ pb: '54px' }}>
       <Box>
-        <Typography variant="h3" component="h1" gutterBottom sx={{ mt: '36px',mb:'44px' , textAlign:'center'}}>
+        <Typography variant="h3" component="h1" gutterBottom sx={{ mt: '36px', mb: '44px', textAlign: 'center' }}>
           Our <span style={{ color: '#6dc396' }}>Testimonials</span>
         </Typography>
       </Box>
       <Box>
         <Slider {...settings} style={{ margin: "12px" }}>
-          {testimonials.map((testimonial) => (
-            <Box key={testimonial.id} sx={{ px: '6px' }}>  {/* Adding padding to each box to create a gap */}
+          {testimonials.map((testimonial, index) => (
+            <Box key={`${testimonial.id}-${index}`} sx={{ px: '6px' }}>
               <Card sx={{ textAlign: 'center', minHeight: '310px', backgroundColor: '#14132b', color: 'white' }}>
                 <CardContent sx={{ minHeight: '200px' }}>
                   <Typography variant="body1" component="p" gutterBottom sx={{ color: 'white' }}>
@@ -71,7 +70,7 @@ const Testimonials = async () => {
                 </CardContent>
                 <CardHeader
                   avatar={
-                    <Avatar alt={testimonial.fields} src={'https:' + testimonial.fields.avatar.fields.file.url} /> 
+                    <Avatar alt={testimonial.fields} src={'https:' + testimonial.fields.avatar.fields.file.url} />
                   }
                   title={
                     <Typography sx={{ color: 'white' }}>
