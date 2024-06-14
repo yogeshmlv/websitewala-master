5f9c0ab0656170903bad9bdcc074284438200715
Yogesh <yogesh@YOGESHs-MacBook-Air.local>
2024-06-14 22:25:02 +0530

    tawk-widget-added

--- websitewala/src/app/layout.js
+++ websitewala/src/app/layout.js
@@ -1,9 +1,10 @@
 import { Prompt } from "next/font/google";
 import Head from "next/head"; // Import Head from next/head
+import Script from 'next/script'; // Import Script from next/script
 import "./globals.css";
 import Navbar from "@/component/navbar/Navbar";
 import Footer from "@/component/footer/Footer";
-import { metadata} from "./contact/metadata"; // Import metadata from ContactPage
+import { metadata } from "./contact/metadata"; // Import metadata from ContactPage
 
 const prompt = Prompt({ weight: '200', subsets: ["latin"] });
 
@@ -19,9 +20,22 @@ export default function RootLayout({ children }) {
           <Navbar />
           {children}
         </div>
-         <div>
-           <Footer />
-          </div>
+        <Footer />
+
+        {/* Add the Tawk.to script here */}
+        <Script id="tawk.to-widget" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/666538bd9a809f19fb3baf6d/1hvtmb9sj';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
